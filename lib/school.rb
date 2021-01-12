0ddf9c4fea808f31a85b366b6b8435c5e9820d24
Sam Ferebee <sam.ferebee@gmail.com>
2021-01-12 15:19:25 -0500

    commit tree cbffe93abcb66c741dc95f1a37477f8235904986

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+
+
+class School
+
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student_name, grade)
+        if @roster.has_key?(grade)
+            @roster[grade].push(student_name)
+        else
+            @roster[grade] = [student_name]
+        end
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        @roster.each{|key, value| roster[key].sort!}
+    end
+
+end
